d75a68190984b4f2a9594c6ff1a958cb15d9a6f8
aniudashi <aniudshi@gmail.com>
2025-08-24 11:42:06 +0800

    提交

--- main.go
+++ main.go
@@ -1,11 +1,20 @@
+// 包申明
 package main
 
-import {
+//引入
+import (
 	"fmt"
 	"os"
+)
+
+func printString(s string) {
+	fmt.Println(s)
 }
+
+var s1 string = "test2"
 
 func main() {
 	fmt.Println(os.Args)
 	fmt.Println("测试go 程序")
+	printString(s1)
 }
